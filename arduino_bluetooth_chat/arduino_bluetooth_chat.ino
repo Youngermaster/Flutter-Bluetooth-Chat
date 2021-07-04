ae88bd67db7e8fb758cc0fb28b603616b300f8bb
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-07-04 15:09:29 -0500

    Flutter Bluetooth serial added

--- arduino_bluetooth_chat/arduino_bluetooth_chat.ino
+++ arduino_bluetooth_chat/arduino_bluetooth_chat.ino
@@ -1,11 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial MyBlue(2, 3);  // RX | TX
+int flag = 0;
+int LED = 8;
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    MyBlue.begin(9600);
+    pinMode(LED, OUTPUT);
+    Serial.println("Ready to connect\nDefualt password is 1234 or 000");
 }
-
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+    if (MyBlue.available())
+        flag = MyBlue.read();
+    if (flag == 1) {
+        digitalWrite(LED, HIGH);
+        Serial.println("LED On");
+    } else if (flag == 0) {
+        digitalWrite(LED, HIGH);
+        Serial.println("LED Off");
+    }
 }
